215e956034968ac94b2043dbfa8f3193577845d9
charizardmigo <fomechepriestly7@gmail.com>
2021-09-27 17:45:28 +0100

    checks a number's divisibility with 5 and 11

---
+++ basic/divisibility.c
@@ -0,0 +1,28 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: CHECKS IF A NUMBER IS DIVISIBLE BY 5 AND/OR 11
+//DATE: 22ND JULY, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a;
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    if ((a % 5) == 0 && (a % 11) == 0)
+        printf("%d is divisible by 5 and 11", a);
+
+    else if ((a % 5) != 0 && (a % 11) != 0)
+        printf("%d is neither divisible by 5 nor 11", a);
+
+    else if ((a % 5) == 0 && (a % 11) != 0)
+        printf("%d is divisible by 5 but not divisible by 11", a);
+
+    else
+        printf("%d is not divisible by 5 but divisible by 11", a);
+
+    return 0;
+}
