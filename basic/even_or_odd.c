be9836d9b197112f2c56e326abb8fc4e9c8ca8d3
charizardmigo <fomechepriestly7@gmail.com>
2021-09-28 13:55:44 +0100

    checks if a number is even or odd

---
+++ basic/even_or_odd.c
@@ -0,0 +1,43 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: CHECKS IF A NUMBER IS EVEN OR ODD
+//DATE: 22ND JULY, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int week;
+
+    printf("Enter week number: ");
+    scanf("%d", &week);
+
+    switch(week){
+       case 1: printf("The day of the week is Monday");
+               break;
+
+       case 2: printf("The day of the week is Tuesday");
+               break;
+
+       case 3: printf("The day of the week is Wednesday");
+               break;
+
+       case 4: printf("The day of the week is Thursday");
+               break;
+
+       case 5: printf("The day of the week is Friday");
+               break;
+
+
+       case 6: printf("The day of the week is Saturday");
+               break;
+
+       case 7: printf("The day of the week is Sunday");
+               break;
+
+       default: printf("INVALID INPUT");
+                break;
+    }
+
+    return 0;
+}
