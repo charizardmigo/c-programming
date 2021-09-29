58ae9b3f812204dd402c9e9ab4b1737bb4eb3a56
charizardmigo <fomechepriestly7@gmail.com>
2021-09-29 08:51:42 +0100

    prints max mnumber

---
+++ basic/max.c
@@ -0,0 +1,25 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: PRINTS GREATEST NUMBER AMONG 3
+//DATE: 22ND JULY, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int a, b, c;
+
+    printf("Enter three numbers(separated with spaces): ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a > b && a > c)
+        printf("Max number is %d", a);
+
+    else if (b > a && b > c)
+        printf("Max number is %d", b);
+
+    else
+        printf("Max number is %d", c);
+
+    return 0;
+}
