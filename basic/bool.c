7fc76869db36032deed2438671cd42dcebfe6e9a
charizardmigo <fomechepriestly7@gmail.com>
2021-10-03 07:59:48 +0100

    returns true if sum of 2 integers is 30

---
+++ basic/bool.c
@@ -0,0 +1,26 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: RETURNS TRUE IF AN INTEGER AMONG TWO OR THEIR SUM EQUALS 30
+//DATE: 30TH SEPTEMBER, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void)
+{
+    int i, j;
+    const int x = 30;
+
+    printf("Enter an integer: ");
+    scanf("%d", &i);
+    printf("Enter another integer: ");
+    scanf("%d", &j);
+
+    if (i == x || j == x || i + j == x)
+        printf("True");
+
+    else
+        printf("False");
+
+    return 0;
+}
