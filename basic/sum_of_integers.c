7f07d50002db1fe59e56c62e47498cdcc0caced3
charizardmigo <fomechepriestly7@gmail.com>
2021-10-04 00:30:58 +0100

    sums two integers

---
+++ basic/sum_of_integers.c
@@ -0,0 +1,22 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: SUMS 2 INTEGERS AND RETURNS TRIPLE THEIR SUM IF INTEGERS ARE THE SAME
+//DATE: 30TH SEPTEMBER, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num1, num2, sum;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num1);
+    printf("Enter another integer: ");
+    scanf("%d", &num2);
+
+    sum = num1 == num2 ? (num1 + num2) * 3 : num1 + num2;
+
+    printf("%d", sum);
+
+    return 0;
+}
