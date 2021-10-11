1d9bebea3710c8a8f798a05e19210f8a15cd8878
charizardmigo <fomechepriestly7@gmail.com>
2021-10-11 22:37:04 +0100

    calculates product of all digits in a number

---
+++ basic/product_digits.c
@@ -0,0 +1,25 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: PRINTS MULTIPLICATION TABLE OF ANY NUMBER
+//DATE: 10TH OCTOBER, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, num;
+    long result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("\nThe Multiplication Table is:\n");
+
+    for (i = 10; i <= 22; i++){
+        result = num * i;
+        printf("\n%d x %d = %ld", num, i, result);
+    }
+    printf("\n");
+
+    return 0;
+}
