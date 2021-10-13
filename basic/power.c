7115972d6ff320c87af61904aa980324d0cfed6e
charizardmigo <fomechepriestly7@gmail.com>
2021-10-13 21:21:10 +0100

    calculates the power of a given number

---
+++ basic/power.c
@@ -0,0 +1,24 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: FINDS POWER OF A NUMBER
+//DATE: 13TH OCTOBER, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num, pow, i, result = 1;
+
+    printf("Input Base: ");
+    scanf("%d", &num);
+    printf("Input Exponent: ");
+    scanf("%d", &pow);
+
+    for (i = 1; i <= pow; i++){
+        result *= num;
+    }
+
+    printf("\n%d ^ %d = %d", num, pow, result);
+
+    return 0;
+}
