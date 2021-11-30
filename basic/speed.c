ca9f0b161aae74ee0f24869ce26b7a88ea6bdc36
charizardmigo <fomechepriestly7@gmail.com>
2021-11-30 08:05:18 +0100

    calculates average speed of a car

---
+++ basic/speed.c
@@ -0,0 +1,22 @@
+//NAME: PRIESTLEY FOMECHE
+//AIM: PROGRAM TO CALCULATE AVERAGE SPEED OF A CAR
+//DATE: 30th NOVEMBER, 2021
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    float distance, speed, time;
+
+    printf("Enter the distance travelled: ");
+    scanf("%f", &distance);
+    printf("Enter the total time taken: ");
+    scanf("%f", &time);
+
+    speed = distance / time;
+
+    printf("The  average speed is %.2f", speed);
+
+    return 0;
+}
