434c6c841aeea55928907d652c56e0da1f6a13ab
charizardmigo <fomechepriestly7@gmail.com>
2022-03-11 22:51:41 +0100

    Checks if an alphabet is a vowel or consonant

--- basic/consonant.c
+++ basic/consonant.c
@@ -25,7 +25,7 @@ int main (void)
     }
 
     else {
-        switch (ch){
+        switch (ch){    
             case 'A': case 'E': case 'I': case 'O': case 'U':
             case 'a': case 'e': case 'i': case 'o': case 'u':
                 printf("%c is a vowel", ch);
