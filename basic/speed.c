56c8644b8fcd39e40265516533760575fcce37f1
charizardmigo <fomechepriestly7@gmail.com>
2022-04-04 00:01:37 +0100

    Calculates average speed of a car

--- basic/speed.c
+++ basic/speed.c
@@ -1,6 +1,6 @@
 //NAME: PRIESTLEY FOMECHE
 //AIM: PROGRAM TO CALCULATE AVERAGE SPEED OF A CAR
-//DATE: 30th NOVEMBER, 2021
+//DATE: 30th NOVEMBER, 2021                 
 
 #include <stdio.h>
 #include <stdlib.h>
