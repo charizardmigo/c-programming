439f9bc5d909ed2c50066e69b68cdb0c36ddc28a
charizardmigo <fomechepriestly7@gmail.com>
2022-04-07 23:19:49 +0100

    Prints multiplication table of any number

--- basic/multiplication_table.c
+++ basic/multiplication_table.c
@@ -15,7 +15,7 @@ int main (void)
 
     printf("\nThe Multiplication Table is:\n");
 
-    for (i = 1; i < 13; i++){
+    for (i = 1; i < 13; i++){                                 
         result = num * i;
         printf("\n%d x %d = %ld", num, i, result);
     }
