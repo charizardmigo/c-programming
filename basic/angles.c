4aeb1df4484c8c11c5b67276c4a9d075d9c751b3
charizardmigo <fomechepriestly7@gmail.com>
2022-05-13 19:36:07 +0100

    Detects if a triangle is valid or not

--- basic/angles.c
+++ basic/angles.c
@@ -2,7 +2,7 @@
 //AIM: DETECTS IF A TRIANGLE IS VALID OR NOT
 //DATE: 22ND JULY, 2021
 
-#include <stdio.h>            
+#include <stdio.h>                                   
 #include <stdlib.h>  
 
 int main (void)     
