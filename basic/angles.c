6aa05a41aa25e7ba71329f8c35a2c1249e842f96
charizardmigo <fomechepriestly7@gmail.com>
2022-05-19 23:40:01 +0100

    Detects if a triangle is valid or not

--- basic/angles.c
+++ basic/angles.c
@@ -14,7 +14,7 @@ int main (void)
     printf("Enter second angle of the triangle: ");                           
     scanf("%f", &ang2);                                          
     printf("Enter third angle of the triangle: ");                
-    scanf("%f", &ang3);            
+    scanf("%f", &ang3);              
 
     sum = ang1 + ang2 + ang3;     
  
