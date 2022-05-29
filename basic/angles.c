d0d90be763581dcd55078b9585d3965f2c1a92d4
charizardmigo <fomechepriestly7@gmail.com>
2022-05-29 22:07:14 +0100

    Detects if a triangle is valid or not

--- basic/angles.c
+++ basic/angles.c
@@ -19,7 +19,7 @@ int main (void)
     sum = ang1 + ang2 + ang3;             
      
     if (sum == 180)                          
-        printf("The triangle is valid");   
+        printf("The triangle is valid");     
             
     else      
         printf("Invalid triangle");                     
