0faa76215349c88b64a4630e9d5980a0fd096a9c
charizardmigo <fomechepriestly7@gmail.com>
2022-07-16 23:32:21 +0100

    Detects if a triangle is valid or not

--- basic/angles.c
+++ basic/angles.c
@@ -22,7 +22,7 @@ int main (void)
         printf("The triangle is valid");                 
                
     else            
-        printf("Invalid triangle");                              
+        printf("Invalid triangle");                                
                                 
 }   
        
