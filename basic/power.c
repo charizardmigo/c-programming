47f9e43d43621eb0d69b22586bebb7cdb961edc1
charizardmigo <fomechepriestly7@gmail.com>
2022-08-27 23:22:46 +0100

    Prints a number in reverse

--- basic/power.c
+++ basic/power.c
@@ -9,7 +9,7 @@ int main(void)
 {
     int num, pow, i, result = 1;
 
-    printf("InaBase: ");
+    printf("IaBase: ");
     scanf("%d", &num);
     printf("Input a Exponent: ");
     scanf("%d", &pow);
