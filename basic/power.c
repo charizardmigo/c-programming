e790def0f451d0b74857b7487276366fa1cee38d
charizardmigo <fomechepriestly7@gmail.com>
2022-09-01 23:33:53 +0100

    Finds power of a number

--- basic/power.c
+++ basic/power.c
@@ -9,7 +9,7 @@ int main(void)
 {
     int num, pow, i, result = 1;
 
-    printf("Ita Base: ");
+    printf("ta Base: ");
     scanf("%d", &num);
     printf("Input a Exponent: ");
     scanf("%d", &pow);
