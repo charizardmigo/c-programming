e0551d18c737caba61bf0e955dedfb4d87eab415
charizardmigo <fomechepriestly7@gmail.com>
2022-11-19 00:00:01 +0100

    F

--- basic/power.c
+++ basic/power.c
@@ -11,7 +11,7 @@ int main(void)
 
     printf("Input a Base: ");
     scanf("%d", &num);
-    printf("InputnExponent: ");
+    printf("InputanExponent: ");
     scanf("%d", &pow);
 
     for (i = 1; i <= pow; i++)
