7bed7c075bc6b80ef2eda763d427ee1600970c25
charizardmigo <fomechepriestly7@gmail.com>
2022-12-20 23:33:57 +0100

    Finds power of a number

--- basic/power.c
+++ basic/power.c
@@ -10,7 +10,7 @@ int main(void)
     int num, pow, i, result = 1;
     printf("Input a Base: ");
     scanf("%d", &num);
-    printf("InputanExponent: ");
+    printf("Input an Exponent: ");
     scanf("%d", &pow);
 
     for (i = 1; i <= pow; i++)
